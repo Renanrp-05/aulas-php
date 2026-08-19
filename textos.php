2218f137dbfd873d9a1401597354540f4d3f2728
Renanrp-05 <ren4nrodrigues05@gmail.com>
2026-08-19 11:14:11 -0300

    Inclusão de Recursos

---
+++ textos.php
@@ -0,0 +1,7 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ducimus molestias rem sapiente cupiditate tempora numquam quod id nostrum. Nulla!</p>
+
+<?php include "lista.html" ?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero expedita at ex beatae soluta repellendus id autem, nostrum earum ratione.</p>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maxime cupiditate quibusdam distinctio quod doloribus temporibus omnis nam quos iste.</p>
