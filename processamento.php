ab3cbe26b953a774b1b5f451f54d286f7c567054
Renanrp-05 <ren4nrodrigues05@gmail.com>
2026-08-20 11:52:08 -0300

    Configurando o recebimento e processamento dos dados

--- processamento.php
+++ processamento.php
@@ -13,6 +13,35 @@
     <div class="container">
         <h1>Recebimento e processamento dos dados</h1>
         <hr>
+
+        <?php
+            // var_dump($_POST);
+
+            // Capturando os dados de cada campo
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $idade = $_POST["idade"];
+            $mensagem = $_POST["mensagem"];
+
+            /* Operador ?? -> coalescência nula */
+            /* Caso nenhum interesse seja selecionado, a variável guardará um array vazio */
+            $interesses = $_POST["interesses"] ?? []; // Array
+
+            /* Caso nenhuma opção seja selecionada, o valor "nao" fica como padrão */
+            $informativos = $_POST["informativos"] ?? "nao"; 
+        ?>
+
+        <h2>Dados recebidos</h2>
+        <p>Nome: <?= $nome ?></p>
+        <p>E-mail: <?= $email ?></p>
+        <p>Idade: <?= $idade ?> anos</p>
+        <p>Mensagem: <?= $mensagem ?></p>
+
+        <?php if(!empty($interesses)): ?>
+        <p>Interesses: <?= implode(",", $interesses) ?></p>
+        <?php endif; ?>
+
+        <p>Informativos: <?= $informativos === 'sim' ? "Sim" : "Não" ?></p>
     </div>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
